3d6de1a2cac67e062e8a9b6438fc2cea2dba47c0
van9a <matyushko.vanya.195@gmail.com>
2018-11-21 18:43:45 +0200

    commit

---
+++ app/js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.client__slider').slick({
+        dots: true,
+        accessibility: false,
+        infinite: true,
+        speed: 3000,
+        slidesToShow: 1,
+        adaptiveHeight: false,
+        autoPlay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+    });
+});
