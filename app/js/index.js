26457c3af12d655734d61c0574ea666f0ce989b2
van9a <matyushko.vanya.195@gmail.com>
2018-12-05 16:07:21 +0200

    commit

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,37 @@
 $(document).ready(function () {
+    $('.hamburg-menu').click(function () {
+        $('.hamburg-menu').toggleClass('active');
+    })
+});
+$(document).ready(function () {
+    $('.hamburg-menu').click(function () {
+        $('#menu').toggleClass('open');
+    })
+});
+window.onscroll = function () {
+    myFunction()
+};
+//header fixed
+var header = document.getElementById("navigation");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+$(document).ready(function () {
     $('.client__slider').slick({
         dots: true,
         accessibility: false,
         infinite: true,
-        speed: 3000,
+        speed: 1500,
         slidesToShow: 1,
         adaptiveHeight: false,
         autoPlay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 1500,
         arrows: false,
     });
 });
